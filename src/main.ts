c414a5f4810afc27521e0fa8a84c3dc591f09dfd
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-05-26 12:22:53 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import Particles from "vue3-particles";
+import App from "./App.vue";
+
+createApp(App).use(Particles).mount("#app");
---
+++ src/shim-dts.d.ts
@@ -0,0 +1,1 @@
+declare module "vue3-particles"; // this won't be necessary after 2.1.0
